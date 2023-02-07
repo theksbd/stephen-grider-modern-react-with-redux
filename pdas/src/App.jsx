12fac89674a808c84ac82b7151050ee9b27452ac
Do Thien Hoang <theksbd@gmail.com>
2023-02-07 13:56:47 +0700

    Lecture 39 Last Bit of Styling

--- pdas/src/App.jsx
+++ pdas/src/App.jsx
@@ -1,13 +1,48 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+import 'bulma/css/bulma.css';
 
 const App = () => {
   return (
     <div>
-      <div>Person Digital Assistants</div>
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>Person Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard title='Alexa' handle='@alexa90' />
-      <ProfileCard title='Cortana' handle='@cortana56' />
-      <ProfileCard title='Siri' handle='@siri01' />
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Alexa'
+                handle='@alexa90'
+                image={AlexaImage}
+                description='Alexa was created by Amazon to help you buy things'
+              />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Cortana'
+                handle='@cortana56'
+                image={CortanaImage}
+                description='Cortana was created by Microsoft to help you manage your computer'
+              />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Siri'
+                handle='@siri01'
+                image={SiriImage}
+                description='Siri was created by Apple to help you control your phone'
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
