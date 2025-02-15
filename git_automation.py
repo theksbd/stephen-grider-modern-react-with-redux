5e769b406af30425789210c62ab32ba5d9cda514
Do Thien Hoang <theksbd@gmail.com>
2025-02-15 20:11:52 +0700

    update git_automation script

--- git_automation.py
+++ git_automation.py
@@ -13,8 +13,23 @@ subprocess.run(["git", "status"])
 # Run git add . 
 subprocess.run(["git", "add", "."])
 
-# Run git commit -m "update"
-subprocess.run(["git", "commit", "-m", "update"])
+# Get the commit message from user input
+# There are 2 options:
+# 1. update
+# 2. Update course process: Section <section> - Lecture <lecture>
+print("Enter the commit message:")
+print("1. update")
+print("2. Update course process: Section <section> - Lecture <lecture>")
+commit_message = input("Enter the number of the commit message (1 or 2) or enter to type your own custom message: ")
+if commit_message == "1":
+    commit_message = "update"
+elif commit_message == "2":
+    commit_message = "Update course process: Section " + input("Enter the section number: ") + " - Lecture " + input("Enter the lecture number: ")
+else:
+    commit_message = input("Enter the custom commit message: ")
+
+# Run git commit with the custom message
+subprocess.run(["git", "commit", "-m", commit_message])
 
 # Run git push
 subprocess.run(["git", "push"])
