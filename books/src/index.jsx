6b7cfecf3434127bac872b894d21d3ebf33128e3
Do Thien Hoang <theksbd@gmail.com>
2025-03-08 19:14:46 +0700

    Update course process: Section 6 - Lecture 119

--- books/src/index.jsx
+++ books/src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
